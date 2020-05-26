1129c78a47cba4c7d3de5e3e6063be1c8c27c476
Eve Reichmann <ereichmann4@aol.com>
2020-05-26 14:07:54 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  answers = ["yass", "yes", "no", "maybe"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  answers = ["yass", "yes", "no", "maybe"]
+  answers << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  answers = ["yass", "yes", "no", "maybe"]
+  answers.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  answers = ["wow","yass", "yes", "no", "maybe", "arrays!"]
+  answers.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  answers = ["wow","yass", "yes", "no", "maybe"]
+  answers.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  answers = ["wow","yass", "am", "yes", "no", "maybe"]
+  answers[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  answers = ["wow","yass", "am", "yes", "no", "maybe"]
+  answers[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  answers = ["wow","yass", "yes", "no", "maybe", "arrays!"]
+  answers[5]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  answers = ["wow","yass", "yes", "no", "maybe", "arrays!"]
+  answers[4] = "totally"
 end
